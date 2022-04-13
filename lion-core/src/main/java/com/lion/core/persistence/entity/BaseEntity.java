e0cef79e5274e0aafca9c7ea19f0a5cdf1e6ff3f
mr.liu <158442534@qq.com>
2022-04-13 15:50:34 +0800

    优化

--- lion-core/src/main/java/com/lion/core/persistence/entity/BaseEntity.java
+++ lion-core/src/main/java/com/lion/core/persistence/entity/BaseEntity.java
@@ -1,12 +1,13 @@
 package com.lion.core.persistence.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.lion.core.common.enums.Delete;
 import com.lion.core.persistence.Validator;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-import org.hibernate.annotations.DynamicInsert;
-import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.*;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -25,13 +26,12 @@ import java.time.LocalDateTime;
  */
 @MappedSuperclass
 @Data
-
 @DynamicInsert
 @EntityListeners(AuditingEntityListener.class)
-@JsonIgnoreProperties(ignoreUnknown = true,value = {"createDateTime","updateDateTime","createUserId","updateUserId"})
+@JsonIgnoreProperties(ignoreUnknown = true,value = {"createDateTime","updateDateTime","createUserId","updateUserId","tenantId"})
 public abstract class BaseEntity implements Serializable {
 
-    protected static final long serialVersionUID = -90000050L;
+    private static final long serialVersionUID = -90000050L;
 
     @Id()
 //    @GeneratedValue(generator = "snow_flake_id")
@@ -42,33 +42,36 @@ public abstract class BaseEntity implements Serializable {
     @Column(name = "id")
 //    @NotNull(message="ID不能为空",groups= {Validator.Update.class, Validator.Delete.class})
     @Schema(description = "ID")
-    private Long id;
+    protected Long id;
 
-//    @Column(name = "is_delete", nullable = false,  columnDefinition = " bit(1) default b'0' comment '是否删除（逻辑删除标记）'")
-//    @Convert(converter = DeleteConverter.class)
-//    private Delete isDelete;
+    @Column(name = "is_delete", nullable = false,  columnDefinition = " bit(1) default b'0' comment '是否删除（逻辑删除标记）'")
+    @Convert(converter = Delete.DeleteConverter.class)
+    private Delete isDelete = Delete.FALSE;
 
     @CreatedDate
     @Column(name = "create_date_time", updatable = false)
     @JsonFormat(pattern = "YYYY-MM-dd HH:mm:ss")
-    private LocalDateTime createDateTime;
+    protected LocalDateTime createDateTime;
 
     @LastModifiedDate
     @Column(name = "update_date_time", insertable = false)
     @JsonFormat(pattern = "YYYY-MM-dd HH:mm:ss")
-    private LocalDateTime updateDateTime;
+    protected LocalDateTime updateDateTime;
 
     @CreatedBy
     @Column(name = "create_user_id", updatable = false)
-    private Long createUserId;
+    protected Long createUserId;
 
     @LastModifiedBy
     @Column(name = "update_user_id", insertable = false)
-    private Long updateUserId;
+    protected Long updateUserId;
 
-    @Column(name = "version")
 //    @NotNull(message="版本号不能为空",groups= {Validator.Update.class})
     @Schema(description = "版本号（修改需要传version,新增不需要传）")
     @Version
-    private Long version;
+    protected Long version;
+
+    @Schema(description = "租户id")
+    @JsonIgnore
+    protected Long tenantId;
 }
