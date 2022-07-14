fe256a867c8262e28fd9ba820592c3ee5f742430
Mr.Liu <158442534@qq.com>
2022-07-14 13:50:03 +0800

    优化

--- lion-core/src/main/java/com/lion/config/RestTemplateConfiguration.java
+++ lion-core/src/main/java/com/lion/config/RestTemplateConfiguration.java
@@ -1,5 +1,7 @@
 package com.lion.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
